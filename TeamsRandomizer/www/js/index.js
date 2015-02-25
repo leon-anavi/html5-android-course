0469436236a17d6d3bfca04840e629a6a7318405
Ani Krivova <anjelika.k08@gmail.com>
2015-02-25 22:13:31 +0200

    Task ready

--- TeamsRandomizer/www/js/index.js
+++ TeamsRandomizer/www/js/index.js
@@ -1,21 +1,36 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+function teams(){
+  var playerarr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+  shuffle(playerarr);
+  console.log(playerarr);
+
+  var taskarr=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+  shuffle(taskarr);
+  console.log(taskarr);
+
+  var nTaskId = 0;
+  for (var i = 0; i < playerarr.length; i+=2) {
+    console.log(playerarr[i] + ' vs '+ playerarr[i+1] + ' Task: '+ taskarr[nTaskId]);
+    nTaskId += 1;
+  }
+}
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,6 +49,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        teams();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
