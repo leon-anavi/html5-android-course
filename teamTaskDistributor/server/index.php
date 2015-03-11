757d40c7f0ecb8b51e55908ba83705c0b7b11aad
Georgi Mihaylov <cocomen@gmail.com>
2015-03-11 20:37:06 +0200

    Remove one folder

--- teamTaskDistributor/server/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$name = (isset($_REQUEST['name'])) ? $_REQUEST['name'] : '';
-$age = (isset($_REQUEST['age'])) ? $_REQUEST['age'] : 0;
-$data = array('name' => $name, 'age' => $age);
-echo json_encode($data);
-?>
